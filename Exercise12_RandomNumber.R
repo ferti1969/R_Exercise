45df93c765a908d9086b20b61da090f083dfea90
Kang Han <ferti1969@gmail.com>
2018-04-27 04:46:23 +0900

    20180427 Update

--- Exercise12_RandomNumber.R
+++ Exercise12_RandomNumber.R
@@ -1,12 +1,12 @@
 #Exercise 12 _ Random Number
 
-#Update : 2018-02-17, Kang Han.
+#Update : 2018-04-27, Kang Han.
 
 options("scipen" = 100)
 
 floor(83.7503)
 
-7 + (2*6) + 1
+7 + (2*6) + 1 #나에게 필요한 난수 자릿수.
 
 #최소 1부터 99999999999999999999 사이의 난수 1개를 발생시키고 내림.
 floor(runif(1, min=1, max=99999999999999999999))
